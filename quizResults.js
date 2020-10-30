625c0bda6b70170cb8c376da21b3a6516139a643
LlsLopez <36282497+LlsLopez@users.noreply.github.com>
2020-10-29 19:27:42 -0700

    Add files via upload

---
+++ quizResults.js
@@ -0,0 +1,16 @@
+var take = document.getElementById("take");
+var create = document.getElementById("create");
+
+
+// button actions
+take.onclick = function() {
+    
+    // change link to redirect to appropriate file
+    location.href = "https://www.youtube.com/";
+}
+
+create.onclick = function() {
+    
+    // change link to redirect to appropriate file
+    location.href = "https://www.youtube.com/";
+}
